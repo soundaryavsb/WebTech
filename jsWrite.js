d94abb1f0f8979d25357c9596e1656cf5d89f074
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2026-07-01 21:46:39 +0530

    Initial Commit - JS2

---
+++ jsWrite.js
@@ -0,0 +1,13 @@
+        document.write("External - I am doc line 1")
+        document.write("<br>")
+        document.write("External - I am doc line 2")
+        document.write("<br>")
+        document.write("External - I am doc line 3")
+
+        //---------console.log----------
+        console.log("External - I am console line 1")
+        console.log("External - I am console line 2")
+
+        //---------alert-------
+        alert("External - I am alert 1")
+        alert("External - I am alert 2")
